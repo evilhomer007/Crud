275902f67cb8b497e76a8595904d6e39038c37e3
Homer <tkennedy@Homers-Mac-Pro.local>
2014-10-27 03:28:36 -0700

    This is the Frist Rev of this Project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Homer on 10/27/14.
+//  Copyright (c) 2014 Stone Dog Digital Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessgeTextField: UITextField!
+    
+    
+    @IBOutlet weak var mailButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessgeTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessgeTextField.text = ""
+        enterMessgeTextField.resignFirstResponder()
+        
+        mailButton.setTitle("It's Gone Baby!", forState: UIControlState.Normal)
+        
+        
+        }
+
+}
